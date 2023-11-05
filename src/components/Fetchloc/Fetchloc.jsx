8dd30c8943f1d7656e44d367c800b0d573367abb
nisha <nishayadav25487062gmail.com>
2023-11-05 20:37:51 +0530

    changes

--- src/components/Fetchloc/Fetchloc.jsx
+++ src/components/Fetchloc/Fetchloc.jsx
@@ -1,31 +1,33 @@
-import React, { useState } from 'react';
-import './Fetchloc.css';
+import React, { useState } from "react";
+import "./Fetchloc.css";
 import cross_icon from "../assests/cross.png";
-import nodata from "../assests/no_data_found.png"
+import nodata from "../assests/no_data_found.png";
+
 
 function Fetchloc() {
   const [locationInfo, setLocationInfo] = useState(null);
   const [zipCode, setZipCode] = useState("");
   const [loading, setLoading] = useState(false);
+  const api = "c1088540-7beb-11ee-a88a-3d740c96d128";
 
   const handleClick = async () => {
     if (!zipCode) return;
-
     setLoading(true);
-
     try {
       const res = await fetch(
-        `https://api.geonames.org/postalCodeLookupJSON?postalcode=${zipCode}&username=nisha_codes`
+        `https://app.zipcodebase.com/api/v1/search?apikey=${api}&codes=${zipCode}`
       );
-
+      console.log();
       if (res.ok) {
         const data = await res.json();
-        setLocationInfo(data.postalcodes?.[0] || { postalcode: "No Data Available" });
+        setLocationInfo(
+          data.results[zipCode][0] || { postalcode: "No Data Available" }
+        );
       } else {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
       setLocationInfo({ postalcode: "No Data Available" });
     } finally {
       setLoading(false);
@@ -46,7 +48,7 @@ function Fetchloc() {
 
   return (
     <div className="body">
-      <div className={`box ${loading ? 'loading' : ''}`}>
+      <div className={`box ${loading ? "loading" : ""}`}>
         <div className="inputData">
           <input
             type="text"
@@ -56,32 +58,33 @@ function Fetchloc() {
           />
           {zipCode && (
             <span className="closeIcon" onClick={handleReset}>
-              <img className='icon_cross' src={cross_icon} alt="Cross Icon" />
+              <img className="icon_cross" src={cross_icon} alt="Cross Icon" />
             </span>
           )}
         </div>
 
         {loading && (
-          <img src="https://media.giphy.com/media/8agqybiK5LW8qrG3vJ/giphy.gif" alt="Loading Indicator" className="loadingIndicator" />
+          <img
+            src="https://media.giphy.com/media/8agqybiK5LW8qrG3vJ/giphy.gif"
+            alt="Loading Indicator"
+            className="loadingIndicator"
+          />
         )}
 
-        {locationInfo && (
-          locationInfo.postalcode === "No Data Available" ? (
+        {locationInfo &&
+          (locationInfo.postalcode === "No Data Available" ? (
             <img src={nodata} alt="No Data Found" className="noDataImage" />
           ) : (
-            <div className='info'>
+            <div className="info">
               <h3>Location Info</h3>
-              <p>Place Name: {locationInfo.placeName}</p>
-              <p>State: {locationInfo.adminName1}</p>
-              <p>Country: {locationInfo.countryCode}</p>
+              <p>Place Name: {locationInfo.province}</p>
+              <p>State: {locationInfo.state}</p>
+              <p>Country: {locationInfo.country_code}</p>
             </div>
-          )
-        )}
+          ))}
 
         {!locationInfo && !loading && (
-          <button onClick={handleClick}>
-            Get Location
-          </button>
+          <button onClick={handleClick}>Get Location</button>
         )}
       </div>
     </div>
